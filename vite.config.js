467ebb7efda68ef15f2c949a0886de0c52b56e2f
Ibrahim Isa Jajere <ibrahimisajajere@gmail.com>
2022-01-28 08:13:47 +0100

    vite production bug walk around

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import cjs from 'rollup-plugin-cjs';
+
+const path = require("path")
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    process.env.MODE !== "production"
+      ? react({
+          jsxRuntime: "classic",
+        })
+      : react(),
+  ],
+  mode: "development",
+  build: {
+    minify: false,
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        index: path.resolve(__dirname, "index.html"),
+      },
+      plugins: [cjs()],
+    },
+    commonjsOptions: {
+      exclude: [/./],
+    },
+  },
+});
