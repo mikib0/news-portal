bc5468ad9e9a1244a58242f37ec7c93fb856bef9
Ibrahim Isa Jajere <ibrahimisajajere274@gmail.com>
2022-05-28 18:18:04 +0100

    build issue workaround

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import cjs from 'rollup-plugin-cjs';
 
-const path = require("path")
+const { resolve } = require('path');
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    process.env.MODE !== "production"
-      ? react({
-          jsxRuntime: "classic",
-        })
-      : react(),
-  ],
-  mode: "development",
+  plugins: [react()],
+
+  /* build configuration: from https://github.com/vitejs/vite/issues/2139#issuecomment-866743932 &
+    https://github.com/vitejs/vite/issues/2139#issuecomment-813135348 
+    to workaround buld issue.
+    
+    also related to /home/mikibo/Desktop/Coding/WebDev/Projects/React/news-portal/node_modules/.pnpm/dialog-polyfill@0.4.10/node_modules/dialog-polyfill/dialog-polyfill.js line 740-742 
+  */
   build: {
-    minify: false,
-    sourcemap: true,
     rollupOptions: {
-      input: {
-        index: path.resolve(__dirname, "index.html"),
-      },
       plugins: [cjs()],
     },
     commonjsOptions: {
